c5d78bab68eb0dae2fae6f89b908f98f3994b831
Josh Garner <jgarner413@gmail.com>
2021-04-02 23:25:55 -0400

    updated landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,13 @@ import '../App.css';
 function Landing() {
   return (
     <div>
-      <h2>This is the Landing page</h2>
+      			<img src="/imgs/asl_landing.png" alt="Learn2Sign logo" />
+			<h2>Welcome to Learn2Sign!</h2>
+			<p className="intro">
+				Learn2Sign! is our CS-545 final project to help
+				users find learn sign language. Users can learn american sign language through our learn tab. They can then test themselves
+        in our Tests tab. Our application is designed to be educational and easy to learn!{' '}
+			</p>
     </div>
   );
 }
