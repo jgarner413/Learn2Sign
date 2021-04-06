02cf49806469ff402de5bd0e9c0553afe4f22ea0
msgyankee <msgyankee@gmail.com>
2021-04-06 11:54:43 -0400

    Express, Mongo, and Routes

---
+++ data/users.js
@@ -0,0 +1,138 @@
+  
+const collections = require('./collections');
+const users = collections.users;
+
+module.exports = {
+
+    //Returns the Database ID of created user OR rejected promise on error
+    async create(userID) {
+        if(!userID || typeof userID !== 'string') return Promise.reject('Must provice a valid userID');
+
+        if(await this.userExists(userID)) return Promise.reject('UserID already in use');
+
+        const userCollection = await users();
+        let newUser = {
+            userID: userID,
+            lessonOne: false,
+            lessonTwo: false,
+            lessonThree: false,
+            testOne: "N/A",
+            testTwo: "N/A",
+            testThree: "N/A"
+        }
+
+        const insertInfo = await userCollection.insertOne(newUser);
+        if(insertInfo.insertedCount === 0) return Promise.reject(`Could not add user ${userID}`);
+
+        return insertInfo.insertedId;
+    },
+
+    //Returns array of all stored user objects
+    async getAllUsers(){
+        const userCollection = await users();
+        const arr = await userCollection.find({}).toArray();
+        return arr;
+    },
+
+    //Returns True or False OR rejected promise as error
+    async userExists(userID){
+        if(!userID || typeof userID !== 'string') return Promise.reject('Must provice a valid userID');
+
+        const userCollection = await users();
+        const user = await userCollection.findOne({userID: userID});
+        if(user === null) return false;
+        return true;
+    },
+
+    //Returns NULL or user object OR rejected promise as error
+    async getUser(userID){
+        if(!userID || typeof userID !== 'string') return Promise.reject('Must provice a valid userID');
+
+        const userCollection = await users();
+        const user = await userCollection.findOne({userID: userID});
+        if(user === null) return null;
+        return user;
+    },
+
+    //Returns true/false for completion OR rejected promise as error
+    async getLesson(userID, lesson){
+        if(!userID || typeof userID !== 'string') return Promise.reject('Must provice a valid userID');
+        if(!lesson || typeof lesson !== 'string') return Promise.reject('Must provide a lesson to get');
+
+        const userCollection = await users();
+        const user = await userCollection.findOne({userID: userID});
+        if(user === null) return Promise.reject("Unable to find user");
+        if(user[lesson] === undefined) return Promise.reject("This lesson does not exist");
+        return user[lesson]
+
+    },
+
+    //Returns the lesson received as echo OR rejected promise as error
+    async setLesson(userID, lesson){
+        if(!userID || typeof userID !== 'string') return Promise.reject('Must provice a valid userID');
+        if(!lesson || typeof lesson !== 'string') return Promise.reject('Must provide a lesson to get');
+
+        const userCollection = await users();
+        const user = await userCollection.findOne({userID: userID});
+
+        if(user === null) return Promise.reject("Unable to find user");
+        if(user[lesson] === undefined) return Promise.reject("This lesson does not exist");
+        let updatedUser = {
+            userID: userID,
+            lessonOne: user.lessonOne,
+            lessonTwo: user.lessonTwo,
+            lessonThree: user.lessonThree,
+            testOne: user.testOne,
+            testTwo: user.testTwo,
+            testThree: user.testThree
+        }
+        updatedUser[lesson] = true;
+
+        const updatedInfo = await userCollection.updateOne({userID: userID}, {$set: updatedUser});
+        if(updatedInfo.modifiedCount === 0) return Promise.reject('Could not update Lesson successfully');
+        return updatedUser[lesson];
+
+    },
+
+    //Returns saved test score, N/A if not taken, OR rejected promise as error
+    async getTest(userID, test){
+        if(!userID || typeof userID !== 'string') return Promise.reject('Must provice a valid userID');
+        if(!test || typeof test !== 'string') return Promise.reject('Must provide a test to get');
+
+        const userCollection = await users();
+        const user = await userCollection.findOne({userID: userID});
+
+        if(user === null) return Promise.reject("Unable to find user");
+        if(user[test] === undefined) return Promise.reject("This test does not exist");
+        return user[test]
+    },
+
+    //Returns the score received as echo OR rejected promise as error
+    async setTest(userID, test, score){
+        if(!userID || typeof userID !== 'string') return Promise.reject('Must provice a valid userID');
+        if(!test || typeof test !== 'string') return Promise.reject('Must provide a test to get');
+        if(!score || typeof score !== 'string') return Promise.reject('Must provide a valid test score to save');
+
+        const userCollection = await users();
+        const user = await userCollection.findOne({userID: userID});
+
+        if(user === null) return Promise.reject("Unable to find user");
+        if(user[test] === undefined) return Promise.reject("This test does not exist");
+
+        let updatedUser = {
+            userID: userID,
+            lessonOne: user.lessonOne,
+            lessonTwo: user.lessonTwo,
+            lessonThree: user.lessonThree,
+            testOne: user.testOne,
+            testTwo: user.testTwo,
+            testThree: user.testThree
+        }
+        updatedUser[test] = score;
+
+        const updatedInfo = await userCollection.updateOne({userID: userID}, {$set: updatedUser});
+        if(updatedInfo.modifiedCount === 0) return Promise.reject('Could not update Test successfully');
+        return score;
+    }
+
+}
---
+++ express.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const app = express();
+const data = require("./data");
+const userData = data.users;
+const cors = require("cors");
+//const bodyParser = require('body-parser');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+
+//Create user object in database
+//EX: localhost:9000/create/mgorman
+app.post("/create/:userID", async (req, res) =>{
+    try{
+        if(!req.params.userID){console.log("No userID"); res.status(400).json({"e":"No User ID"});}
+        await userData.create(req.params.userID);
+        res.status(200).json({});
+    }
+    catch(e){
+        console.log(e);
+        res.status(500).json({"e":"Already in use"});
+    }
+});
+
+//Get lesson completion
+//EX: localhost:9000/mgorman/lessonOne
+app.get("/:userID/:lesson", async (req, res) => {
+    try{
+        if(!req.params.userID){console.log("No userID"); res.status(400).json({"e":"No User ID"});}
+        if(!req.params.lesson){console.log("No lesson"); res.status(400).json({"e":"No Lesson specified"});}
+
+        let status = await userData.getLesson(req.params.userID, req.params.lesson);
+        res.status(200).json({"status":status});
+    }
+    catch(e){
+        console.log(e);
+        res.status(500).json({"e":"Server Error"});
+    }
+});
+
+//Set lesson completion to TRUE
+//EX: localhost:9000/mgorman/lessonOne
+app.post("/:userID/:lesson", async(req, res) => {
+    try{
+        if(!req.params.userID){console.log("No userID"); res.status(400).json({"e":"No User ID"});}
+        if(!req.params.lesson){console.log("No lesson"); res.status(400).json({"e":"No Lesson specified"});}
+
+        let status = await userData.setLesson(req.params.userID, req.params.lesson);
+        res.status(200).json({"status":status});
+    }
+    catch(e){
+        console.log(e);
+        res.status(500).json({"e":"Server Error"});
+    }
+});
+
+//Get test score for User
+//EX: localhost:9000/mgorman/testOne
+app.get("/:userID/:test", async (req, res) => {
+    try{
+        if(!req.params.userID){console.log("No userID"); res.status(400).json({"e":"No User ID"});}
+        if(!req.params.test){console.log("No test"); res.status(400).json({"e":"No Test specified"});}
+
+        let score = await userData.getTest(req.params.userID, req.params.test);
+        res.status(200).json({"score":score});
+    }
+    catch(e){
+        console.log(e);
+        res.status(500).json({"e":"Server Error"});
+    }
+});
+
+//Set test score for User
+//EX localhost:9000/mgorman/testOne/83
+app.post("/:userID/:test/:score", async (req, res) => {
+    try{
+        if(!req.params.userID){console.log("No userID"); res.status(400).json({"e":"No User ID"});}
+        if(!req.params.test){console.log("No test"); res.status(400).json({"e":"No Test specified"});}
+        if(!req.params.score){console.log("No score"); res.status(400).json({"e":"No score specified"});}
+
+        let score = await userData.setTest(req.params.userID, req.params.test, req.params.score);
+        res.status(200).json({"score":score});
+    }
+    catch(e){
+        console.log(e);
+        res.status(500).json({"e":"Server Error"});
+    }
+});
+
+app.listen(9000, () => {
+    console.log("Server is running!");
+    console.log("Your routes will be running on http://localhost:9000");
+  });
+  
