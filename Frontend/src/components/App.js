2be616a1d6920c71875b8950ba90f277af96d7da
TP170996 <55114902+TP170996@users.noreply.github.com>
2021-04-12 17:59:21 -0400

    alphabets lessons added

--- Frontend/src/components/App.js
+++ Frontend/src/components/App.js
@@ -8,6 +8,8 @@ import Navigation from './Navigation';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Test from './TestDashboard';
+import lessons from './lessons';
+import Alphabets from './Alphabets';
 import { AuthProvider } from '../firebase/Auth';
 import PrivateRoute from './PrivateRoute';
 import Test1 from './Tests/Test1';
@@ -25,6 +27,9 @@ function App() {
         <PrivateRoute path="/tests" component={Test} />
         <PrivateRoute path="/test/1" component={Test1} />
         <PrivateRoute path="/account" component={Account} />
+        <PrivateRoute path="/lessons/:id" component={lessons} />
+
+        
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
       </Router>
