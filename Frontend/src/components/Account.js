218eaf4e17de2478849759f3e51dd2907971c8b0
Josh Garner <jgarner413@gmail.com>
2021-04-20 19:52:10 -0400

    added account image, name, and email to account page

--- Frontend/src/components/Account.js
+++ Frontend/src/components/Account.js
@@ -2,15 +2,39 @@ import React from 'react';
 import SignOutButton from './SignOut';
 import '../App.css';
 import ChangePassword from './ChangePassword';
+import firebase from 'firebase';
+import AccountImage from '../Account_Image.png';
+import { Container, Typography, ButtonGroup } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 function Account() {
-  return (
-    <div>
-      <h2>Account Page</h2>
-      <ChangePassword />
-      <SignOutButton />
-    </div>
-  );
+	console.log(firebase.auth().currentUser);
+	return (
+		<div>
+			<h2>Account Page</h2>
+			<img src={AccountImage} alt="user"></img>
+			<Typography variant="h3" className="AccountInfo">
+				{firebase.auth().currentUser.displayName}
+			</Typography>
+			<br />
+			<Typography variant="h4" className="AccountInfo">
+				{firebase.auth().currentUser.email}
+			</Typography>
+			<br />
+			<Container className="AccountInfo">
+				<ChangePassword />
+				<ButtonGroup
+					variant="contained"
+					color="alternate"
+					aria-label="contained primary button group"
+				>
+					<Button>
+						<SignOutButton />
+					</Button>
+				</ButtonGroup>
+			</Container>
+		</div>
+	);
 }
 
 export default Account;
